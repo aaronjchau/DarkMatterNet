f3ba66a0a01950e17abeb2164b76ff8e7ac48cac
Aaron Chau <aaron@Aarons-MacBook-Pro-3.local>
2018-12-31 15:08:38 -0800

    Add data input & filtering for NYU dataset

--- DMN_Data.py
+++ DMN_Data.py
@@ -3,120 +3,138 @@ import pandas as pd
 import tensorflow as tf
 
 
-
 # [Reminder] List of column names assigned when the Illustris dataset is loaded
-CSV_COLUMN_NAMES = ['Ignored_Index',
-                    'SubhaloBHMass',
-                    'SubhaloBHMdot',
-                    'SubhaloGasMetallicity',
-                    'SubhaloGasMetallicityHalfRad',
-                    'SubhaloGasMetallicityMaxRad',
-                    'SubhaloGasMetallicitySfr',
-                    'SubhaloGasMetallicitySfrWeighted',
-                    'SubhaloGrNr',
-                    'SubhaloHalfmassRad',
-                    'SubhaloIDMostbound',
-                    'SubhaloLen',
-                    'SubhaloMass',
-                    'SubhaloMassInHalfRad',
-                    'SubhaloMassInMaxRad',
-                    'SubhaloMassInRad',
-                    'SubhaloParent',
-                    'SubhaloSFR',
-                    'SubhaloSFRinHalfRad',
-                    'SubhaloSFRinMaxRad',
-                    'SubhaloSFRinRad',
-                    'SubhaloStarMetallicity',
-                    'SubhaloStarMetallicityHalfRad',
-                    'SubhaloStarMetallicityMaxRad',
-                    'SubhaloStellarPhotometricsMassInRad',
-                    'SubhaloStellarPhotometricsRad',
-                    'SubhaloVelDisp',
-                    'SubhaloVmax',
-                    'SubhaloVmaxRad',
-                    'SubhaloWindMass',
-                    'SubhaloSublinkID',
-                    'SubhaloHalfmassRadType0',
-                    'SubhaloHalfmassRadType1',
-                    'SubhaloHalfmassRadType2',
-                    'SubhaloHalfmassRadType3',
-                    'SubhaloHalfmassRadType4',
-                    'SubhaloHalfmassRadType5',
-                    'SubhaloMassInHalfRadType0',
-                    'SubhaloMassInHalfRadType1',
-                    'SubhaloMassInHalfRadType2',
-                    'SubhaloMassInHalfRadType3',
-                    'SubhaloMassInHalfRadType4',
-                    'SubhaloMassInHalfRadType5',
-                    'SubhaloMassInMaxRadType0',
-                    'SubhaloMassInMaxRadType1',
-                    'SubhaloMassInMaxRadType2',
-                    'SubhaloMassInMaxRadType3',
-                    'SubhaloMassInMaxRadType4',
-                    'SubhaloMassInMaxRadType5',
-                    'SubhaloMassInRadType0',
-                    'SubhaloMassInRadType1',
-                    'SubhaloMassInRadType2',
-                    'SubhaloMassInRadType3',
-                    'SubhaloMassInRadType4',
-                    'SubhaloMassInRadType5',
-                    'SubhaloMassType0',
-                    'SubhaloMassType1',
-                    'SubhaloMassType2',
-                    'SubhaloMassType3',
-                    'SubhaloMassType4',
-                    'SubhaloMassType5',
-                    'SubhaloStellarPhotometricsU',
-                    'SubhaloStellarPhotometricsB',
-                    'SubhaloStellarPhotometricsV',
-                    'SubhaloStellarPhotometricsK',
-                    'SubhaloStellarPhotometricsg',
-                    'SubhaloStellarPhotometricsr',
-                    'SubhaloStellarPhotometricsi',
-                    'SubhaloStellarPhotometricsz',
-                    'SubhaloID',
-                    'B300',
-                    'B1000',
-                    'TotalSFRMass']
+ILLUSTRIS_COLUMN_NAMES = ['Pre_Drop_Index',
+                            'SubhaloBHMass',
+                            'SubhaloBHMdot',
+                            'SubhaloGasMetallicity',
+                            'SubhaloGasMetallicityHalfRad',
+                            'SubhaloGasMetallicityMaxRad',
+                            'SubhaloGasMetallicitySfr',
+                            'SubhaloGasMetallicitySfrWeighted',
+                            'SubhaloGrNr',
+                            'SubhaloHalfmassRad',
+                            'SubhaloIDMostbound',
+                            'SubhaloLen',
+                            'SubhaloMass',
+                            'SubhaloMassInHalfRad',
+                            'SubhaloMassInMaxRad',
+                            'SubhaloMassInRad',
+                            'SubhaloParent',
+                            'SubhaloSFR',
+                            'SubhaloSFRinHalfRad',
+                            'SubhaloSFRinMaxRad',
+                            'SubhaloSFRinRad',
+                            'SubhaloStarMetallicity',
+                            'SubhaloStarMetallicityHalfRad',
+                            'SubhaloStarMetallicityMaxRad',
+                            'SubhaloStellarPhotometricsMassInRad',
+                            'SubhaloStellarPhotometricsRad',
+                            'SubhaloVelDisp',
+                            'SubhaloVmax',
+                            'SubhaloVmaxRad',
+                            'SubhaloWindMass',
+                            'SubhaloSublinkID',
+                            'SubhaloHalfmassRadType0',
+                            'SubhaloHalfmassRadType1',
+                            'SubhaloHalfmassRadType2',
+                            'SubhaloHalfmassRadType3',
+                            'SubhaloHalfmassRadType4',
+                            'SubhaloHalfmassRadType5',
+                            'SubhaloMassInHalfRadType0',
+                            'SubhaloMassInHalfRadType1',
+                            'SubhaloMassInHalfRadType2',
+                            'SubhaloMassInHalfRadType3',
+                            'SubhaloMassInHalfRadType4',
+                            'SubhaloMassInHalfRadType5',
+                            'SubhaloMassInMaxRadType0',
+                            'SubhaloMassInMaxRadType1',
+                            'SubhaloMassInMaxRadType2',
+                            'SubhaloMassInMaxRadType3',
+                            'SubhaloMassInMaxRadType4',
+                            'SubhaloMassInMaxRadType5',
+                            'SubhaloMassInRadType0',
+                            'SubhaloMassInRadType1',
+                            'SubhaloMassInRadType2',
+                            'SubhaloMassInRadType3',
+                            'SubhaloMassInRadType4',
+                            'SubhaloMassInRadType5',
+                            'SubhaloMassType0',
+                            'SubhaloMassType1',
+                            'SubhaloMassType2',
+                            'SubhaloMassType3',
+                            'SubhaloMassType4',
+                            'SubhaloMassType5',
+                            'SubhaloStellarPhotometricsU',
+                            'SubhaloStellarPhotometricsB',
+                            'SubhaloStellarPhotometricsV',
+                            'SubhaloStellarPhotometricsK',
+                            'SubhaloStellarPhotometricsg',
+                            'SubhaloStellarPhotometricsr',
+                            'SubhaloStellarPhotometricsi',
+                            'SubhaloStellarPhotometricsz',
+                            'SubhaloID',
+                            'B300',
+                            'B1000',
+                            'TotalSFRMass']
 
+# [Reminder] List of column names assigned when the NYU dataset is loaded
+# NOTE: NYU column names must match Illustris column names
+NYU_COLUMN_NAMES = ['Pre_Drop_Index',
+                    'SubhaloStellarPhotometricsMassInRad',
+                    'SubhaloGasMetallicity',
+                    'B300',
+                    'B1000']
 
 
 
 def raw_dataframe():
-    """Function which loads entire Illustris dataset"""
+    """Function which loads Illustris and NYU datasets"""
 
-    # NOTE: Illustris_V2.csv pre-filtering: halos with 0 stellar mass (PhotometricsMassInRad) are removed
     # Load data from Illustris_V2.csv and reassign column names
-    df = pd.read_csv("Illustris_V2.csv",
+    #   NOTE: Illustris_V2.csv pre-filtering: halos with 0 stellar mass (PhotometricsMassInRad) were dropped
+    iDF = pd.read_csv("Illustris_V2.csv",
                         header=0,
-                        names=CSV_COLUMN_NAMES,
-                        dtype=np.float64
-                    )
+                        names=ILLUSTRIS_COLUMN_NAMES,
+                        dtype=np.float64)
 
-    return df
+    # Load data from NYU.csv and reassign column names
+    #   NOTE: NYU.csv pre-filtering: halos with 0 stellar mass were dropped
+    nDF = pd.read_csv("NYU.csv",
+                        header=0,
+                        names=NYU_COLUMN_NAMES,
+                        dtype=np.float64)
+
+    return iDF, nDF
 
 
 
 def load_data(label_name='SubhaloMassInRad', train_fraction=0.8, seed=None):
-    """Function which loads the Illustris dataset and returns Train Set and Test Set dataframes"""
+    """Function which loads Illustris, NYU datasets; returns filtered Train & Test Set for Illustris, Predict Set for NYU"""
 
-    # Use raw_dataframe() to load entire Illustris dataset
-    data = raw_dataframe()
+    # Use raw_dataframe() to load Illustris and NYU datasets
+    iData, nData = raw_dataframe()
 
-    # Remove halos with stellar mass < 10^8 Mstar
-    data_stellar_cut = data.drop(data[data.SubhaloStellarPhotometricsMassInRad < 0.01].index)
+    # Illustris: remove halos with stellar mass < 10^8 Mstar
+    iData_stell_cut = iData.drop(iData[iData.SubhaloStellarPhotometricsMassInRad < 0.01].index)
+
+    # NYU: remove halos with stellar mass < 10^8 Mstar
+    nData_stell_cut = nData.drop(nData[nData.SubhaloStellarPhotometricsMassInRad < 0.01].index)
 
     # Split Illustris dataframe randomly into a Train Set (80% of data) and a Test Set (20% of data)
     np.random.seed(seed)
-    train_features = data_stellar_cut.sample(frac=train_fraction, random_state=seed)
-    test_features = data_stellar_cut.drop(train_features.index)
+    train_features = iData_stell_cut.sample(frac=train_fraction, random_state=seed)
+    test_features = iData_stell_cut.drop(train_features.index)
 
     # Remove Halo Mass (label) from Train and Test Set and assign to new dataframes
     train_label = train_features.pop(label_name)
     test_label = test_features.pop(label_name)
 
+    # Clean name
+    NYU_features = nData_stell_cut
+
     # Return a pair of dataframes (features only, label only) for Train Set, Test Set
-    return (train_features, train_label), (test_features, test_label)
+    return (train_features, train_label), (test_features, test_label), NYU_features
 
 
 
